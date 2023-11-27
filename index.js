93df1afa9b2fe8be1cd774cad5be029995939d5b
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2023-11-28 00:42:02 +0100

    open a wildcard for cross origin access

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import 'dotenv/config';
 import { connectDb } from "./configs/db.js";
 import { router as main} from "./router/main.js";
 const app = express();
+import cors from "cors";
 const PORT = process.env.PORT || 3001 ; 
 
 //connecting to the Database 
 connectDb();
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors({origin:'*'})); 
 
 //the api main routes 
 app.use('/api',main);
